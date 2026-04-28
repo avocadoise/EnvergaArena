514ddc7ae3c0a1b3d97bede83e3823522f09973e
lucifron28 <lucifron28@gmail.com>
2026-04-28 14:18:00 +0800

    feat: register AIRecap model in admin with display and filter options

--- backend/rooney/admin.py
+++ backend/rooney/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RooneyQueryLog
+from .models import AIRecap, RooneyQueryLog
 
 
 @admin.register(RooneyQueryLog)
@@ -14,3 +14,10 @@ class RooneyQueryLogAdmin(admin.ModelAdmin):
     def question_preview(self, obj):
         return obj.question[:80]
     question_preview.short_description = 'Question'
+
+
+@admin.register(AIRecap)
+class AIRecapAdmin(admin.ModelAdmin):
+    list_display = ['generated_title', 'trigger_type', 'scope_type', 'status', 'event', 'generated_at', 'reviewed_at']
+    list_filter = ['status', 'trigger_type', 'scope_type']
+    search_fields = ['generated_title', 'generated_summary', 'scope_key']
